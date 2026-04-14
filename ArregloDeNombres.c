e194804267836c61077821c0dc911f54055141ca
Balbo Tomás <anubisbalbo@gmail.com>
2026-04-14 15:55:17 -0300

    funcion de la branch

--- ArregloDeNombres.c
+++ ArregloDeNombres.c
@@ -4,6 +4,7 @@
 
 
 void MostrarPersonas(char** V);
+void BuscarNombre(char** V, int ind);
 int main (){
     char* buff = (char *)malloc(50*sizeof(char));
     char* vector[5];
@@ -15,6 +16,11 @@ int main (){
         strcpy(vector[i],buff);
     }
     MostrarPersonas(vector);
+
+    puts("Ingrese un numero del 0 al 4 para buscar el nombre: ");
+    int num;
+    scanf("%d",&num);
+    BuscarNombre(vector,num);
     free(buff);
     for (int i = 0; i < 5; i++){
         free(vector[i]);
@@ -25,5 +31,13 @@ void MostrarPersonas(char** V){
     for (int i = 0;i<5; i++){
         puts("El nombre es: ");
         printf(" %s\n",V[i]);
+    }
+}
+void BuscarNombre(char** V, int ind){
+    if (ind <0 || ind > 4) {
+        puts("no se encontró el valor buscado");
+    }else {
+    puts("El nombre en la posicion buscada es: ");
+    printf(" %s\n",V[ind]);
     }
 }
