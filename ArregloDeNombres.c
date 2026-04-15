9aafdf32d677ab54359c5001aa0f883efd7db0f3
anubiscdll <anubisbalbo@gmail.com>
2026-04-14 21:20:52 -0300

    Punto 2 realizado

--- ArregloDeNombres.c
+++ ArregloDeNombres.c
@@ -16,7 +16,31 @@ int main() {
         strcpy(vector[i], buff);
     }
     MostrarPersonas(vector);
-
+    puts("Elija la operacion a realizar:");
+    puts("1. Busqueda de nombre por ID");
+    puts("2. Busqueda de nombre por palabra");
+    puts("Ingrese el numero de la operación que desea realizar: ");
+    int menu;
+    scanf("%d", &menu);
+    if (menu < 1 || menu > 2) {
+        puts("Operación no encontrada");
+    } else if (menu == 1) {
+        puts("Ingrese un numero del 0 al 4 para buscar el nombre: ");
+        int num;
+        scanf("%d", &num);
+        BuscarNombrePorId(vector, num);
+    } else {
+        puts("Ingrese un nombre a buscar: ");
+        gets(buff);
+        int busc = BuscarNombrePorPalabra(vector, buff);
+        if (busc == -1) {
+            puts("No se encontro el nombre");
+        } else {
+            puts("Se encontro:");
+            printf(" %s\n", vector[busc]);
+        }
+    }
+    /*
     puts("Ingrese un numero del 0 al 4 para buscar el nombre: ");
     int num;
     scanf("%d", &num);
@@ -30,10 +54,12 @@ int main() {
         puts("Se encontro:");
         printf(" %s\n", vector[busc]);
     }
+    */
     free(buff);
     for (int i = 0; i < 5; i++) {
         free(vector[i]);
     }
+
     return 0;
 }
 void MostrarPersonas(char** V) {
